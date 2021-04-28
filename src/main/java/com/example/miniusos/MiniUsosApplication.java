11ba56d190c5e16b36ac394eff4d3209a07c02fd
Szpiler <50781614+Szpiler@users.noreply.github.com>
2021-04-28 22:00:25 +0200

    Starting Commit

---
+++ src/main/java/com/example/miniusos/MiniUsosApplication.java
@@ -0,0 +1,13 @@
+package com.example.miniusos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniUsosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniUsosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/miniusos/MiniUsosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniusos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniUsosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

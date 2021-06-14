c9c0d7a2faae220ab46e193109088af4310e8a5a
kori14k <kori09@wp.pl>
2021-06-15 01:31:51 +0200

    trying dolaczdoprzedmiot

---
+++ src/main/java/com/example/miniusos/model/DolaczDoPrzedmiotForm.java
@@ -0,0 +1,20 @@
+package com.example.miniusos.model;
+
+public class DolaczDoPrzedmiotForm {
+
+    private int obecna_ilosc_studentow;
+    private Student student;
+    private int id_przycisk;
+
+
+    public int getObecna_ilosc_studentow() {
+        return obecna_ilosc_studentow;
+    }
+
+    public void setObecna_ilosc_studentow(int obecna_ilosc_studentow) {
+        this.obecna_ilosc_studentow = obecna_ilosc_studentow;
+    }
+
+
+
+}
---
+++ src/main/java/com/example/miniusos/service/DolaczDoPrzedmiotService.java
@@ -0,0 +1,38 @@
+package com.example.miniusos.service;
+
+
+import com.example.miniusos.model.PracownikNaukowy;
+import com.example.miniusos.model.PracownikDziekanatu;
+import com.example.miniusos.model.RegisterForm;
+import com.example.miniusos.model.Student;
+import com.example.miniusos.model.User;
+import com.example.miniusos.model.DolaczDoPrzedmiotForm;
+import com.example.miniusos.service.DolaczDoPrzedmiotService;
+import com.example.miniusos.repositories.PrzedmiotRepository;
+import com.example.miniusos.repositories.UserRepository;
+import com.example.miniusos.model.Uzytkownik;
+import com.example.miniusos.repositories.PracownikDziekanatuRepository;
+import com.example.miniusos.repositories.PracownikNaukowyRepository;
+import com.example.miniusos.repositories.StudentRepository;
+import com.example.miniusos.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+@Service
+public class DolaczDoPrzedmiotService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Autowired
+    private PrzedmiotRepository przedmiotRepository;
+
+    public Uzytkownik dolaczDoPrzedmiot(DolaczDoPrzedmiotForm dolaczDoPrzedmiotForm) {
+
+
+    }
+}

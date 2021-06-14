30f36030336031d9f67ac3efdc00469e569ea89c
Piotrek <piotrek.wiktor555@gmail.com>
2021-06-14 22:04:59 +0200

    Login, security controllerd

--- src/main/java/com/example/miniusos/controller/LoginController.java
+++ src/main/java/com/example/miniusos/controller/LoginController.java
@@ -1,14 +1,26 @@
 package com.example.miniusos.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class LoginController {
+<<<<<<< HEAD
     // Login form
     @RequestMapping("/login")
     public String login() {
+=======
+
+
+    // Login form
+    @RequestMapping("/")
+    public String login()
+    {
+>>>>>>> parent of e837290 (Working logging)
         return "login/login_page";
     }
 

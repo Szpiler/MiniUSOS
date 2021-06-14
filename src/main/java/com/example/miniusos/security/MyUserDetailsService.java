4fcb276f9ecc08ed82af567e8f6377773d7ad4e9
Piotrek <piotrek.wiktor555@gmail.com>
2021-06-14 22:08:37 +0200

    fixing commit 3

---
+++ src/main/java/com/example/miniusos/security/MyUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.example.miniusos.security;
+
+import com.example.miniusos.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import com.example.miniusos.model.User;
+
+import java.util.Optional;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+       Optional<User> user = userRepository.findByUserName(userName);
+
+       user.orElseThrow(() -> new UsernameNotFoundException("Nie znaleziono: " + userName));
+
+       return user.map(MyUserDetails::new).get();
+    }
+}

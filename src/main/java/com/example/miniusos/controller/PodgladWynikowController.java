e095e6f6d5cf3ce6fc25603eb7e511b0a39681af
Piotrek <piotrek.wiktor555@gmail.com>
2021-06-15 13:00:47 +0200

    PodgladWynikow fix

--- src/main/java/com/example/miniusos/controller/PodgladWynikowController.java
+++ src/main/java/com/example/miniusos/controller/PodgladWynikowController.java
@@ -1,16 +1,17 @@
 package com.example.miniusos.controller;
 
+import com.example.miniusos.model.Ocena;
 import com.example.miniusos.model.Student;
 import com.example.miniusos.repositories.OcenaRepository;
 import com.example.miniusos.repositories.StudentRepository;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Controller
 public class PodgladWynikowController {
@@ -26,15 +27,14 @@ public class PodgladWynikowController {
     public String showResults(Model model)
     {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        List<Student> studenci = new ArrayList<>();
-        studentRepository.findAll().forEach(studenci::add);
-        long indeks = -1;
-        for (Student s:studenci)
+        Long indeks = -1L;
+        Optional<Student> student = studentRepository.findByLogin(auth.getPrincipal().toString());
+        student.orElseThrow(() -> new UsernameNotFoundException("Nie znaleziono: "));
+        Set<Ocena> oceny = student.get().getOceny();
+        for(Iterator<Ocena> it = oceny.iterator(); it.hasNext();)
         {
-            if(s.getLogin().equals(auth.getPrincipal()))
-            {
-                indeks = s.getId();
-            }
+            Ocena o = it.next();
+            indeks = o.getId();
         }
         model.addAttribute("wyniks", ocenaRepository.getById(indeks));
         auth.getPrincipal();
--- src/main/java/com/example/miniusos/repositories/StudentRepository.java
+++ src/main/java/com/example/miniusos/repositories/StudentRepository.java
@@ -1,7 +1,12 @@
 package com.example.miniusos.repositories;
 
+import com.example.miniusos.model.PracownikNaukowy;
 import com.example.miniusos.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findByLogin (@Param("login") String login);
 }

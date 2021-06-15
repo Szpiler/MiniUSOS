c5e54dbf7ec707ca2da81038207440845d96c7a8
FLap\Kanaji <pilifok2@gmail.com>
2021-06-15 12:39:45 +0200

    Register view fix and import clean up
    Changed login and phone number

--- src/main/java/com/example/miniusos/service/RegisterService.java
+++ src/main/java/com/example/miniusos/service/RegisterService.java
@@ -1,22 +1,12 @@
 package com.example.miniusos.service;
 
-import com.example.miniusos.model.PracownikNaukowy;
-import com.example.miniusos.model.PracownikDziekanatu;
-import com.example.miniusos.model.RegisterForm;
-import com.example.miniusos.model.Student;
-import com.example.miniusos.model.User;
-import com.example.miniusos.repositories.UserRepository;
-import com.example.miniusos.model.Uzytkownik;
+import com.example.miniusos.model.*;
 import com.example.miniusos.repositories.PracownikDziekanatuRepository;
 import com.example.miniusos.repositories.PracownikNaukowyRepository;
 import com.example.miniusos.repositories.StudentRepository;
 import com.example.miniusos.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-
-import java.util.Iterator;
-import java.util.StringTokenizer;
 
 @Service
 public class RegisterService {

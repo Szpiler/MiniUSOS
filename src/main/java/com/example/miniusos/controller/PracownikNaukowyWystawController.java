e0065bb22c8655940799ff8330bd49e0124b388f
Piotrek <piotrek.wiktor555@gmail.com>
2021-06-15 18:51:59 +0200

    Wystaw Ocene fix

--- src/main/java/com/example/miniusos/controller/PracownikNaukowyWystawController.java
+++ src/main/java/com/example/miniusos/controller/PracownikNaukowyWystawController.java
@@ -3,13 +3,20 @@ package com.example.miniusos.controller;
 import com.example.miniusos.model.PracownikNaukowy;
 import com.example.miniusos.model.Przedmiot;
 import com.example.miniusos.model.Student;
+import com.example.miniusos.model.WystawOceneForm;
 import com.example.miniusos.repositories.PracownikNaukowyRepository;
 import com.example.miniusos.repositories.PrzedmiotRepository;
+import com.example.miniusos.repositories.StudentRepository;
+import com.example.miniusos.service.WystawOceneService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,10 +25,38 @@ import java.util.List;
 public class PracownikNaukowyWystawController {
     private final PracownikNaukowyRepository pracownikNaukowyRepository;
     private final PrzedmiotRepository przedmiotRepository;
+    private final StudentRepository studentRepository;
 
-    public PracownikNaukowyWystawController(PracownikNaukowyRepository pracownikNaukowyRepository, PrzedmiotRepository przedmiotRepository) {
+    @Autowired
+    private WystawOceneService wystawOceneService;
+
+    public PracownikNaukowyWystawController(PracownikNaukowyRepository pracownikNaukowyRepository, PrzedmiotRepository przedmiotRepository, StudentRepository studentRepository) {
         this.pracownikNaukowyRepository = pracownikNaukowyRepository;
         this.przedmiotRepository = przedmiotRepository;
+        this.studentRepository = studentRepository;
+    }
+
+    @RequestMapping("/wystaw")
+    public String wystawOcene() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String role = auth.getAuthorities().toString();
+        if (auth.getAuthorities().toString().equals("[ROLE_STAFF]")) {
+            return "naukowy/oceny/wystaw";
+        } else
+            return "authorize/error";
+    }
+
+    @RequestMapping(value = "/wystaw", method = RequestMethod.POST)
+    public String wystawOcene(@ModelAttribute WystawOceneForm ocena, BindingResult errors, Model model) {
+
+        if (errors.hasErrors()) {
+            // tu podajesz do jakiego widoku chcesz przełączyć przy błędzie (błędy typu niepoprawne dane w formularzu)
+            return "/przedmiots";
+        }
+
+        wystawOceneService.dodaj(ocena);
+
+        return "naukowy/oceny/wystaw";
     }
 
     @RequestMapping("/wystaw")
